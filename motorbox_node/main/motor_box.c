5327bcd5a0802443d351b32bbd25dd8c29e26592
kmarcell0 <kmarcell.kovi@gmail.com>
2026-03-17 16:01:43 +0100

    error handling bugfixes

--- motorbox_node/main/motor_box.c
+++ motorbox_node/main/motor_box.c
@@ -3,8 +3,7 @@
 
 const static char* TAG = "MOTOR_BOX";
 
-// start includes
-
+// --- start includes ---
 #include "solar.h"
 #include "can_manager.h"
 
@@ -16,32 +15,27 @@ const static char* TAG = "MOTOR_BOX";
 #include "freertos/task.h"
 
 #include "driver/mcpwm_prelude.h"
+// --- end includes ---
 
-// end includes
 
-
-// start definitions
-
-#define SETUP_GROUP                  0        // group id of all components of mvpwm
+// --- start definitions ---
+#define SETUP_GROUP                  0        // group id of all components of mcpwm
 #define MAX_THROTTLE                 255      // Maximum value for throttle
 #define MIN_THROTTLE                 0        // Minimum value for throttle
-#define SERVO_TIMEBASE_PERIOD        100      // TODO change this to not template value
-#define SERVO_TIMEBASE_RESOLUTION_HZ 100      // TODO change this to not template value
-#define MOTOR_PWM_GPIO               1        // TODO change this to not template value
 
-
+// JAVÍTVA: Valós értékek standard PWM-hez (50Hz, 20ms periódus, 1us felbontás)
+#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1 tick = 1 microsecund
+#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks = 20ms = 50Hz frissítés
+#define MOTOR_PWM_GPIO               1        // TODO: Állítsd be a valós GPIO pinre!
 
 #define WD_TIMEOUT_MS                500      // watchdog timeout threshold in miliseconds
-
-#define MAX_MOTOR_TEMP               120      // Maximum allowed tempreture for motor operation (TODO set temp correctly, just placeholder for now)
+#define MAX_MOTOR_TEMP               120      // Maximum allowed tempreture for motor operation
 
 #define SAFE_STARTUP_WAIT_MS         2000     // safe startup time in ms (throttle in 0 pos for this amount of time before engine can start)
+// --- end definitions ---
 
-// end definitions
 
-
-// start Frame defines
-
+// --- start Frame defines ---
 // Motor controll frame
 static twai_message_t controll_frame = {
       .identifier = Control_ID,
@@ -67,16 +61,14 @@ static twai_message_t motor_data_frame = {
       .data_length_code = 8,
       .data = {0}
 };
+// --- end Frame defines ---
 
-// end Frame defines
 
-// start Resource allocation and Initialization
-
+// --- start Resource allocation and Initialization ---
 mcpwm_cmpr_handle_t comparator;
 mcpwm_oper_handle_t operator;
 
 void setup_mcpwm() {
-
   // Timer setup
   mcpwm_timer_handle_t timer;
   mcpwm_timer_config_t timer_conf = {
@@ -93,13 +85,13 @@ void setup_mcpwm() {
     .group_id = SETUP_GROUP
   };
   ESP_ERROR_CHECK(mcpwm_new_operator(&oper_conf, &operator));
+  ESP_ERROR_CHECK(mcpwm_operator_connect_timer(operator, timer));
 
   // Comparator setup
   mcpwm_comparator_config_t cmpr_conf = {
     .flags.update_cmp_on_tez = true
   };
   ESP_ERROR_CHECK(mcpwm_new_comparator(operator, &cmpr_conf, &comparator));
-
 
   // Generator setup
   mcpwm_gen_handle_t generator;
@@ -108,6 +100,7 @@ void setup_mcpwm() {
   };
   ESP_ERROR_CHECK(mcpwm_new_generator(operator, &gen_conf, &generator));
 
+  // PWM Logika: Számláló indulásakor HIGH
   ESP_ERROR_CHECK(
       mcpwm_generator_set_action_on_timer_event(
         generator,
@@ -117,6 +110,7 @@ void setup_mcpwm() {
           MCPWM_GEN_ACTION_HIGH
   )));
 
+  // PWM Logika: Amikor eléri a comparator értékét, LOW
   ESP_ERROR_CHECK(
       mcpwm_generator_set_action_on_compare_event(
         generator,
@@ -126,84 +120,73 @@ void setup_mcpwm() {
           MCPWM_GEN_ACTION_LOW
   )));
 
-  /* Not used in the end, might be useful someday...
-
-  // Brake configuration
-  mcpwm_soft_fault_config_t soft_fault_conf = {
-    .flags.active_high = true
-  };
-  mcpwm_fault_handle_t fault_handle;
-  ESP_ERROR_CHECK(mcpwm_new_soft_fault(&soft_fault_conf, &fault_handle));
-  // On fault event force PWM output to LOW
-  mcpwm_generator_set_action_on_brake_event(
-      generator,
-      MCPWM_GEN_BRAKE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_OPER_BRAKE_MODE_OST, MCPWM_GENERATOR_ACTION_LOW)
-  )
-  */
+  // JAVÍTVA: Timer elindítása (enélkül nem generál jelet!)
+  ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+  ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
 }
+// --- end Resource allocation and Initialization ---
 
-// end Resource allocation and Initialization
 
-// Can reciever, validation
+// --- Can reciever, validation ---
 enum valid_errors {
   LOWER_BOUND, UPPER_BOUND, LENGHT, NONE
 };
 volatile enum valid_errors g_valid_fault = NONE;
 
 uint8_t validate_throttle(twai_message_t *control_frame) {
-
-  can_manager_lock_frame(control_frame); // lock frame while reading from it
+  can_manager_lock_frame(control_frame);
 
   // Lenght validation
-  if (control_frame->data_length_code < 8) {
+  if (control_frame->data_length_code < 1) { // 8 helyett 1-re javítva, ha csak 1 byte a gáz
     ESP_LOGE(TAG, "Throttle data lenght is less then expected, raising fault flag");
     g_valid_fault = LENGHT;
+    can_manager_unlock_frame(control_frame);
     return 0;
   }
-  uint8_t raw_val = (uint8_t)control_frame->data[0];
+  
+  uint8_t raw_val = control_frame->data[0];
+  can_manager_unlock_frame(control_frame);
 
-  can_manager_unlock_frame(control_frame); // unlock frame
-
-  // Range validation FIXME compiler says its always false
-  // lower
-  if (raw_val < (uint8_t)MIN_THROTTLE) {
+  // Range validation (A raw_val < MIN_THROTTLE felesleges, ha MIN_THROTTLE = 0 és uint8_t a típus, 
+  // de bennehagyjuk általános esetre, int konverzióval a warning elkerülésére)
+  if ((int16_t)raw_val < MIN_THROTTLE) {
     ESP_LOGE(TAG, "Throttle value is LESS then expected, raising fault flag");
     g_valid_fault = LOWER_BOUND;
     return 0;
   }
-  // upper
-  if (raw_val > (uint8_t)MAX_THROTTLE) {
+  
+  if (raw_val > MAX_THROTTLE) {
     ESP_LOGE(TAG, "Throttle value is MORE then expected, raising fault flag");
     g_valid_fault = UPPER_BOUND;
     return 0;
   }
 
-  // all is good, sending value from frame g->g
-  ESP_LOGI(TAG, "THROTTLE FRAME: \t%u", raw_val);
-  if (g_valid_fault) {
+  // Minden rendben
+  if (g_valid_fault != NONE) {
     g_valid_fault = NONE;
   }
   return raw_val;
 }
 
-// Watchdog
+// --- Watchdog ---
 static uint32_t last_valid_msg_time = 0;
-bool is_communication_timed_out() { // returns true if last_valid_msg_time is larger than WD_TIMEOUT_MS
+bool is_communication_timed_out() { 
   uint32_t now = xTaskGetTickCount() * portTICK_PERIOD_MS;
   return (now - last_valid_msg_time) > WD_TIMEOUT_MS;
 }
 
-// Motor tempreture controll
+// --- Motor tempreture controll ---
 bool is_motor_temp_high(twai_message_t *motor_temps_frame) {
-  can_manager_lock_frame(motor_temps_frame); // lock frame while reading from it
+  can_manager_lock_frame(motor_temps_frame);
 
-  // Lenght validation
-  if (motor_temps_frame->data_length_code < 8) {
-    ESP_LOGE(TAG, "Throttle data lenght is less then expected, raising fault flag");
+  if (motor_temps_frame->data_length_code < 1) {
+    ESP_LOGE(TAG, "Temp data lenght is less then expected");
+    can_manager_unlock_frame(motor_temps_frame);
+    return false; // vagy true, attól függően mennyire szigorú a hiba
   }
-  uint8_t motor_temp = (uint8_t)motor_temps_frame->data[0];
-
-  can_manager_unlock_frame(motor_temps_frame); // unlock frame
+  
+  uint8_t motor_temp = motor_temps_frame->data[0];
+  can_manager_unlock_frame(motor_temps_frame);
 
   if (motor_temp > MAX_MOTOR_TEMP) {
     ESP_LOGE(TAG, "Motor temp of %u is greater then the maximum %u", motor_temp, MAX_MOTOR_TEMP);
@@ -212,30 +195,22 @@ bool is_motor_temp_high(twai_message_t *motor_temps_frame) {
   return false;
 }
 
-/*
- * TODO
- * Read motor data
- */
-
-// Main motor control task loop
+// --- Main motor control task loop ---
 void ts_motorControlLoop(void *pvParameters) {
   // frame registration
-  ESP_ERROR_CHECK(can_manager_register_frame(&controll_frame, /*tx_enable=*/true, /*rx_enable=*/true));
-  ESP_ERROR_CHECK(can_manager_register_frame(&motor_data_frame, /*tx_enable=*/true, /*rx_enable=*/true));
-  ESP_ERROR_CHECK(can_manager_register_frame(&motor_temps_frame, /*tx_enable=*/true, /*rx_enable=*/true));
+  ESP_ERROR_CHECK(can_manager_register_frame(&controll_frame, true, true));
+  ESP_ERROR_CHECK(can_manager_register_frame(&motor_data_frame, true, true));
+  ESP_ERROR_CHECK(can_manager_register_frame(&motor_temps_frame, true, true));
 
   // can manager initialization
-  ESP_ERROR_CHECK(can_manager_init(&g_config, &t_config, &f_config, /*tx_period_ms=*/100, "MOTOR_CTRL"));
+  ESP_ERROR_CHECK(can_manager_init(&g_config, &t_config, &f_config, 100, "MOTOR_CTRL"));
 
   enum states {
     STARTUP_STATE, OPERATION_STATE, CAN_OVER_UART_STATE
   };
   enum states state = STARTUP_STATE;
 
-  uint8_t target_throttle = 1;
-  bool controll_frame_recieved = false;
-  bool temp_frame_recieved = false;
-  bool is_system_cleared = false;
+  uint8_t target_throttle = 0;
   bool waiting_for_zero = false;
   uint32_t zero_throttle_start_time = 0;
 
@@ -244,95 +219,67 @@ void ts_motorControlLoop(void *pvParameters) {
   while (1) {
     uint32_t now = xTaskGetTickCount() * portTICK_PERIOD_MS;
 
-    // motor control (throttle) frame reception
-    controll_frame_recieved = (twai_receive(&controll_frame, 0) == ESP_OK);
-    if (controll_frame_recieved && controll_frame.identifier == THROTTLE_CAN_ID) { // TODO real id of frame
-      target_throttle = validate_throttle(&controll_frame);
-      last_valid_msg_time = xTaskGetTickCount() * portTICK_PERIOD_MS;
-      switch (g_valid_fault) {
-        case LOWER_BOUND:
-          ESP_LOGE(TAG, "Data from throttle is LESS than accepted!");
-          state = STARTUP_STATE;
-          break;
-        case UPPER_BOUND:
-          ESP_LOGE(TAG, "Data from throttle is MORE than accepted!");
-          state = STARTUP_STATE;
-          break;
-        case LENGHT:
-          ESP_LOGE(TAG, "Data from throttle is SHORTER then expected!");
-          state = STARTUP_STATE;
-          break;
-        case NONE:
-          is_system_cleared = true;
-          break;
-      }
+    target_throttle = validate_throttle(&controll_frame);
+    bool temp_is_high = is_motor_temp_high(&motor_temps_frame);
+
+    // TODO: A last_valid_msg_time frissítését érdemes a can_manager RX callbackjébe tenni,
+    // de ideiglenesen frissítjük, ha érvényes gázadatot látunk (vagy ha a can_manager ad rá API-t)
+    if (g_valid_fault == NONE) {
+       last_valid_msg_time = now; 
     }
 
-    // motor temp frame reception
-    temp_frame_recieved = (twai_receive(&motor_temps_frame, 0) == ESP_OK);
-    if (temp_frame_recieved && motor_temps_frame.identifier == MOTOR_TEMP_ID) { // TODO real id of frame
-      last_valid_msg_time = xTaskGetTickCount() * portTICK_PERIOD_MS;
-      if (is_motor_temp_high(&motor_temps_frame)) {
-        state = STARTUP_STATE;
-      } else {
-        is_system_cleared = true;
-      }
+    bool comms_timeout = is_communication_timed_out();
+
+    bool system_fault_active = (g_valid_fault != NONE) || temp_is_high || comms_timeout;
+
+    if (system_fault_active && state == OPERATION_STATE) {
+      ESP_LOGE(TAG, "Motor entering STARTUP_STATE due to system fault!");
+      state = STARTUP_STATE;
     }
 
-    // watchdog error handling
-    if (is_communication_timed_out()) {
-      state = STARTUP_STATE; // TODO switch to CAN_OVER_UART_STATE not startup, COUS still needs to be implemented
-      ESP_LOGE(TAG, "Watchdog: CAN Timeout");
-    } else {
-      is_system_cleared = true;
-    }
-
-    // state handling for safe startup
     switch (state) {
 
-      case STARTUP_STATE: //startup, error state
-        // only proceed to operation s. when throttle is in deadzone for SAFE_STARTUP_WAIT_MS
-        // this part is kind of a clusterduck sorry if you have to maintain this : ^(
-        if (target_throttle == 0 && is_system_cleared) {
+      case STARTUP_STATE: // startup, error state
+        // Motor biztonsági lekapcsolása (0% PWM)
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, 0)); 
 
-          if (!waiting_for_zero) {
-            // start "Timer", might be better if we use a real timer not this bs
-            zero_throttle_start_time = now;
-            waiting_for_zero = true;
-
-          } else if (now - zero_throttle_start_time >= SAFE_STARTUP_WAIT_MS) {
-            // change to oper state and reset
-            state = OPERATION_STATE;
-            waiting_for_zero = false;
-            ESP_LOGI(TAG, "System armed, motor ready for operation");
+        if (!system_fault_active) {
+          if (target_throttle == 0) {
+            if (!waiting_for_zero) {
+              zero_throttle_start_time = now;
+              waiting_for_zero = true;
+            } else if (now - zero_throttle_start_time >= SAFE_STARTUP_WAIT_MS) {
+              state = OPERATION_STATE;
+              waiting_for_zero = false;
+              ESP_LOGI(TAG, "System armed, motor ready for operation");
+            }
+          } else {
+            waiting_for_zero = false; // Gázt adtak a várakozás alatt
           }
-
         } else {
-          // reset if throttle changes from 0
-          waiting_for_zero = false;
+          waiting_for_zero = false; // Hiba van jelen, nem indulhat a timer
         }
-        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, 0)); // Motor kept on 0 while starting up
         break;
 
-      case OPERATION_STATE: //normal operation
-        if (!is_system_cleared) { // checking system faults
-          state = STARTUP_STATE;
-          ESP_LOGE(TAG, "Motor entering STARTUP_STATE due to previous error!");
-          break;
-        }
+      case OPERATION_STATE: // normal operation
+        // PWM kitöltési tényező kiszámítása (0-255 skálázása a Timer periódusra)
+        // Megjegyzés: Ha standard RC jelet használsz, a "0 throttle" általában 1ms (1000 tick), 
+        // a max pedig 2ms (2000 tick). Ha ez ipari motorvezérlő, akkor maradhat a 0-100% (0-20000 tick).
+        uint32_t compare_val = ((uint32_t)target_throttle * SERVO_TIMEBASE_PERIOD) / MAX_THROTTLE;
 
-        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, target_throttle)); // TODO: reverse is not programmed yet, need to know more about the motor controller
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, compare_val)); 
         break;
 
       case CAN_OVER_UART_STATE: // no can communication so switch over to uart
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, 0)); // Motor kikapcs
         // TODO the actual can over uart stuff
         break;
     }
 
-    vTaskDelay(pdMS_TO_TICKS(100)); // TODO adjust timings
+    // A ciklus futási sebessége (10ms = 100Hz frissítés a simább gázreakcióért)
+    vTaskDelay(pdMS_TO_TICKS(10)); 
   }
 };
-
 
 void app_main(void) {
   ESP_LOGI(TAG, "Motor Controller Starting...");
@@ -350,4 +297,3 @@ void app_main(void) {
       ESP_LOGE(TAG, "Failed to create motor control task!");
   }
 }
-
