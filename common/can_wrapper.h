49e1c800ef40eb08988c18d03f5e5538713050ca
elsiyar <elsiyar.islyamova@gmail.com>
2026-03-22 16:29:50 +0100

    Updated can_wrapper.h

--- common/can_wrapper.h
+++ common/can_wrapper.h
@@ -1,23 +1,62 @@
+// CAN wrapper for all nodes in the solarboat
+
 #ifndef CAN_WRAPPER_H
 #define CAN_WRAPPER_H
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "driver/twai.h"
+#include "esp_err.h"
 
-// Define standard pins based on Koxi's solar.h
-#define DEFAULT_CAN_TX PIN_CAN_TX 
-#define DEFAULT_CAN_RX PIN_CAN_RX
+#include "driver/twai.h" 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Setting options so nodes don't have to use  ESP32 macros
+typedef enum {
+    CAN_BAUD_125K,
+    CAN_BAUD_250K,
+    CAN_BAUD_500K, // Both throttle and motorbox nodes use this
+} can_baud_rate_t;
+
+typedef enum {
+    CAN_MODE_NORMAL,       // Regular send/receive
+    CAN_MODE_LISTEN_ONLY,  // Only read, don't send (good for debugging)
+    CAN_MODE_LOOPBACK      // Tests if the ESP32 can talk to itself
+} can_mode_t;
+
+// The main configuration structure
 typedef struct {
-    int tx_io;
-    int rx_io;
-    uint32_t baud_rate; // e.g. 500 for 500kbits
+    int tx_pin;                // TX pin number
+    int rx_pin;                // RX pin number
+    can_baud_rate_t baud_rate; // Speed setting
+    can_mode_t mode;           // Operating mode
 } can_config_t;
 
-// Standard API requested by leader
-bool can_init(const can_config_t *config);
-bool can_send(uint32_t id, const uint8_t *data, uint8_t len);
-bool can_receive(twai_message_t *msg);
 
+// The simple API functions that replace twai_driver_install, twai_start, etc.
+
+// Starts the CAN hardware
+esp_err_t can_init(const can_config_t *config);
+
+// Sends a message. "timeout_ms" is how long to wait if the bus is busy.
+esp_err_t can_send(const twai_message_t *message, uint32_t timeout_ms);
+
+// Waits to receive a message.
+esp_err_t can_receive(twai_message_t *message, uint32_t timeout_ms);
+
+// Shuts down the CAN hardware safely.
+esp_err_t can_deinit(void);
+
+// Checks if the CAN bus crashed due to too many errors (electrical noise, etc.)
+bool can_is_bus_off(void);
+
+// Tries to restart the CAN bus if it crashed.
+esp_err_t can_recover_bus(void);
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif // CAN_WRAPPER_H
